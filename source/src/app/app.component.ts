03ada593ab0c71ebce46cf2d98d7585f685b6754
jakubcywka <39415217+jakubcywka@users.noreply.github.com>
2019-01-28 17:01:24 +0100

    This is the very first stable version of a RunTime app. It has no bugs and errors. Works with any unit you want and has improved ui to get better compatible with mobile devices thanks to Bulma's responsive web design.

--- source/src/app/app.component.ts
+++ source/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public distance: number;
   public time: number;
 
@@ -13,6 +13,8 @@ export class AppComponent {
   public speed: string;
 
   public unit: string;
+
+  public language: string;
 
   public numberToTimeLike(num: string): string {
     if (parseFloat(num) < 10) {
@@ -22,42 +24,53 @@ export class AppComponent {
     }
   }
 
-  public convert(event: KeyboardEvent): void {
-    if (event.key === 'Enter') {
-      if (this.unit === 'Hours' && this.distance !== undefined && this.time !== undefined) {
-        if ((this.time * 60 / this.distance).toString().includes('.') === true) {
-          const paceConstant: string = (this.time * 60 / this.distance).toString();
-          const speedConstant: string = (this.distance / this.time).toFixed(2).toString();
-          // tslint:disable-next-line:radix
-          const minutesConstant: string = paceConstant.substring(0, paceConstant.indexOf('.'));
-          // tslint:disable-next-line:max-line-length
-          const secondsConstant: string = this.numberToTimeLike((parseFloat(paceConstant.substring(paceConstant.indexOf('.'))) * 60).toFixed(0).toString());
-          this.pace = `${minutesConstant}:${secondsConstant} minute(s) / kilometer`;
-          this.speed = `${speedConstant} kilometer(s) / hour`;
-        } else {
-          const paceConstant: string = (this.time * 60 / this.distance).toString();
-          const speedConstant: string = (this.distance / this.time).toString();
-          this.pace = `${paceConstant}:00 minute(s) / kilometer`;
-          this.speed = `${speedConstant} kilometer(s) / hour`;
-        }
-      }
-      if (this.unit === 'Minutes' && this.distance !== undefined && this.time !== undefined) {
-        if ((this.time / this.distance).toString().includes('.') === true) {
-          const paceConstant: string = (this.time / this.distance).toString();
-          const speedConstant: string = (this.distance / (this.time / 60)).toFixed(2).toString();
-          // tslint:disable-next-line:radix
-          const minutesConstant: string = paceConstant.substring(0, paceConstant.indexOf('.'));
-          // tslint:disable-next-line:max-line-length
-          const secondsConstant: string = this.numberToTimeLike((parseFloat(paceConstant.substring(paceConstant.indexOf('.'))) * 60).toFixed(0).toString());
-          this.pace = `${minutesConstant}:${secondsConstant} minute(s) / kilometer`;
-          this.speed = `${speedConstant} kilometer(s) / hour`;
-        } else {
-          const paceConstant: string = (this.time / this.distance).toString();
-          const speedConstant: string = (this.distance / (this.time / 60)).toString();
-          this.pace = `${paceConstant}:00 minute(s) / kilometer`;
-          this.speed = `${speedConstant} kilometer(s) / hour`;
-        }
+  public calculate(): void {
+    if (this.distance.toString().includes(',') === true) {
+      this.distance = parseFloat(parseFloat(this.distance.toString().replace(',', '.')).toPrecision(5));
+    }
+    if (this.unit === 'Hours' && this.distance !== undefined && this.time !== undefined) {
+      if ((this.time * 60 / this.distance).toString().includes('.') === true) {
+        const paceConstant: string = (this.time * 60 / this.distance).toString();
+        const speedConstant: string = (this.distance / this.time).toFixed(2).toString();
+        // tslint:disable-next-line:radix
+        const minutesConstant: string = paceConstant.substring(0, paceConstant.indexOf('.'));
+        // tslint:disable-next-line:max-line-length
+        const secondsConstant: string = this.numberToTimeLike((parseFloat(paceConstant.substring(paceConstant.indexOf('.'))) * 60).toFixed(0).toString());
+        this.pace = `${minutesConstant}:${secondsConstant}`;
+        this.speed = `${speedConstant}`;
+      } else {
+        const paceConstant: string = (this.time * 60 / this.distance).toString();
+        const speedConstant: string = (this.distance / this.time).toString();
+        this.pace = `${paceConstant}:00`;
+        this.speed = `${speedConstant}`;
       }
     }
+    if (this.unit === 'Minutes' && this.distance !== undefined && this.time !== undefined) {
+      if ((this.time / this.distance).toString().includes('.') === true) {
+        const paceConstant: string = (this.time / this.distance).toString();
+        const speedConstant: string = (this.distance / (this.time / 60)).toFixed(2).toString();
+        // tslint:disable-next-line:radix
+        const minutesConstant: string = paceConstant.substring(0, paceConstant.indexOf('.'));
+        // tslint:disable-next-line:max-line-length
+        const secondsConstant: string = this.numberToTimeLike((parseFloat(paceConstant.substring(paceConstant.indexOf('.'))) * 60).toFixed(0).toString());
+        this.pace = `${minutesConstant}:${secondsConstant}`;
+        this.speed = `${speedConstant}`;
+      } else {
+        const paceConstant: string = (this.time / this.distance).toString();
+        const speedConstant: string = (this.distance / (this.time / 60)).toString();
+        this.pace = `${paceConstant}:00`;
+        this.speed = `${speedConstant}`;
+      }
+    }
+  }
+
+  public convert(event: KeyboardEvent): void {
+    if (event.key === 'Enter') {
+      this.calculate();
+    }
+  }
+
+  public ngOnInit() {
+    this.language = navigator.language;
   }
 }
